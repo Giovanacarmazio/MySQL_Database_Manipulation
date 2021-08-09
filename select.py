dec924162411290d8d054880cea07b8b18d9abff
Giovana Carmazio <83242981+Giovanacarmazio@users.noreply.github.com>
2021-08-09 13:54:33 -0300

    Update select.py

--- select.py
+++ select.py
@@ -1,1 +1,13 @@
+import mysql.connector
 
+# Coneção do Banco de Dados #
+mydb = mysql.connector.connect(host="localhost", user="root", password="1234", database="pycodebr")
+
+# Cria Query, executa SQL e captura os dados #
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM clientes")
+clientes = mycursor.fetchall()
+
+# Exibe registros #
+for cliente in clientes:
+    print(cliente)
